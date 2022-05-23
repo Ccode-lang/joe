9ea4db8c47020b2e690ff3eee32f8adc3e44d7c9
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-05-23 15:07:32 -0700

    Server config (#2)
    * Create config.json
    
    * Update main.py
    
    * Update config.json
    
    * Update config.json
    
    * Update config.json
    
    * Update config.json
    
    * Update main.py
    
    * now verbose works

--- server/main.py
+++ server/main.py
@@ -1,34 +1,43 @@
 import os
 import simpmsg
+import json
 
 class InputError(Exception):
     pass
 
-devicelist = {
-    "test" : "0.0.0.0"
-}
-
+with open("config.json") as configdata:
+    config = json.load(configdata)
+    devicelist = config['connections']
+    verbose = config["settings"]["verbose"]
+if verbose:
+    print("Loading server")
 server = simpmsg.simpserve()
 server.initserver("0.0.0.0", 3501)
+if verbose:
+    print("Server loaded, waiting for message.")
 while True:
-    com = server.getmsg()[1]
-    if com.startswith("jmsg:"):
-        args = com.split(":")
+    com = server.getmsg()
+    if verbose:
+        print("Got message \"" + com[1] + "\" from " + com[0])
+    if com[1].startswith("jmsg:"):
+        args = com[1].split(":")
         try:
             if len(args) == 3:
                 try:
                     device = devicelist[args[2]]
                 except:
-                    raise InputError("Device does not exist.")
+                    raise InputError("Device that client tried to contact does not exist.")
                 try:
-                    server.sendmsg(args[1], device, 3500)
+                    if verbose:
+                        print("sending message \"" + args[1] + "\" to device " + args[2])
+                    server.sendmsg("jcom:" + args[1], device, 3500)
                 except Exception as e:
                     print(repr(e))
             else:
-                raise InputError("Too many arguments.")
+                raise InputError("Too many arguments from client.")
         except InputError as e:
-            print(repr(e))
-            pass
+            if verbose:
+                print(repr(e))
     else:
-        print("Invalid message.")
-        pass
+        if verbose:
+            print("Invalid message from client.")
