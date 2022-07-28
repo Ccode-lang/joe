2a0f8553804e6a477c482cfac4abd229e584c09a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-07-28 10:27:22 -0700

    Update commands.py

--- assistant/commands.py
+++ assistant/commands.py
@@ -1,1 +1,19 @@
+import os
+import sys
+import argparse
+home = os.path.expanduser("~")
+prodir = os.path.join(home, "joe", "pro")
+sys.path.append(prodir)
+prodir = os.path.join(home, "joe", "lib")
+sys.path.append(prodir)
 
+import greeting
+import jterminate
+import jtoggle
+
+prolist = {
+    "hello" : greeting.greeting,
+    "exit" : jterminate.exit,
+    "pass" : jpass,
+    "toggle device" : jtoggle.toggle
+}
