d52d56ce23cf1ef190c861def54510ddb18dcc05
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-07-28 10:35:41 -0700

    Update commands.py

--- assistant/commands.py
+++ assistant/commands.py
@@ -5,11 +5,13 @@ home = os.path.expanduser("~")
 prodir = os.path.join(home, "joe", "pro")
 sys.path.append(prodir)
 
-
+# You can add custom commands in this config file
+# To do so add your module to joe/lib and import it here.
 import greeting
 import jterminate
 import jtoggle
-
+# once you do that ad it to this list. The format is:
+# "phrase to activate (all lowercase)" : ModuleName.FunctionToRun
 prolist = {
     "hello" : greeting.greeting,
     "exit" : jterminate.exit,
